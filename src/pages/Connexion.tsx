c5c63d5363ea0b198eaad668f01a7d375d22cf6d
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-11-06 22:31:36 -0500

    Début router

---
+++ src/pages/Connexion.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Connexion: React.FC = () => {
+  return (
+    <div>
+      <h1>Connexion</h1>
+      <p>Page de connexion</p>
+    </div>
+  );
+};
+
+export default Connexion;
---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  ACCUEIL: '/',
+  CONNEXION: '/connexion',
+} as const;
