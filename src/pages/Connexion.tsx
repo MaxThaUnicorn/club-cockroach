8f44237a7d2864c0e98c4229badd46828cc9875b
Joel Tidjane <tidjanejoel@gmail.com>
2024-11-07 09:32:27 -0500

    Ajout de style dans le formulaire de connexion

--- src/pages/Connexion.tsx
+++ src/pages/Connexion.tsx
@@ -3,26 +3,22 @@ import React from 'react';
 const Connexion: React.FC = () => {
   return (
     <div>
+      <div className='formConnexion'>
       <h1>Connexion</h1>
-
       <p>Page de connexion</p>
-
-          <form action="">
-
-            <p>
-              <label htmlFor="">Username:</label>
-              <input type="text" name='username'/>
-            </p>
-            
-            <p>
-              <label htmlFor="">Password:</label>
-              <input type="text" name='password'/>
-            </p>
-
-            <button>Connexion</button>
-
-          </form>
-
+        <form action="">
+          <p>
+            <label htmlFor="">Username:</label>
+            <input type="text" name='username'/>
+          </p>
+          
+          <p>
+            <label htmlFor="">Password:</label>
+            <input type="text" name='password'/>
+          </p>
+          <button>Connexion</button>
+        </form>
+      </div>
     </div>
   );
 };
