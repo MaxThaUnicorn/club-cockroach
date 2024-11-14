231abf2c1cd39f7564d12d38978937622d545ba3
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-11-14 11:01:55 -0500

    Personnage component

--- src/component/Personnage.tsx
+++ src/component/Personnage.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Personnage: React.FC = () => {
   return (
-    <div id="1018820439746478081" style={{width: "50px", overflow: "hidden"}}>
-      <img style={{width: "100%", height: "100%"}} src="/src/assets/img/cockroach.png" alt="" />
-    </div>
+    <div id="1018820439746478081" className='personnage'></div>
   );
 };
 
