0ec763525033751958b8ff9c3b60b5c9eaf16cd4
jaypomaloney <mathisl525@gmail.com>
2024-11-14 13:03:51 -0500

    new lobby

--- src/pages/Salles/Lobby.tsx
+++ src/pages/Salles/Lobby.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Personnage from '../../component/Personnage';
 import Background from '../../assets/img/vieux-plancher-sale.jpg';
 import initializePositions from '../../component/Positions';
+import Messagerie from '../../component/Messagerie';
 
 const Lobby: React.FC = () => {
   var currentUserId = '1018820439746478081';
@@ -14,6 +15,7 @@ const Lobby: React.FC = () => {
     <div className='conteneur-jeu'>
       <div className='contenu-jeu' style={{backgroundImage: `url(${Background})`}}>
         <Personnage userId={currentUserId} />
+        <Messagerie></Messagerie>
       </div>
     </div>
   );
