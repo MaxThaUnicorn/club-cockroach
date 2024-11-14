76f1332f28c53ba943993b4157d291f04f28a9cf
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-11-14 11:04:00 -0500

    Fonctionnement de cors

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -4,7 +4,11 @@ import Personnage from '../component/Personnage';
 const Lobby: React.FC = () => {
   const updatePosition = async () => {
     let data = await fetch('http://localhost:5000/api/positions', {
-      method: 'GET'
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json; charset=utf-8',
+        'Content-Type': 'application/json; charset=utf-8'
+      }
     });
 
     console.log(data);
@@ -16,7 +20,7 @@ const Lobby: React.FC = () => {
   return (
     <div className='conteneur-jeu'>
       <div className='contenu-jeu imageBackgroundLobby'>
-        <div style={{top: '5%', left: '5%'}} className='personnage'></div>
+        <Personnage />
       </div>
     </div>
   );
--- src/server/server.ts
+++ src/server/server.ts
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors); 
+app.use(cors({
+  origin: 'http://localhost:' + process.env.PORT,
+  methods: ['GET', 'POST'],
+})); 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
