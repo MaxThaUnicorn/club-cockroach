9f25e747f1a9c74dc1a96d03ab594200438ec539
Max Belval-Michaud <142822452+MaxThaUnicorn@users.noreply.github.com>
2024-11-14 12:38:17 -0500

    Ajout salles + nom personnage

--- src/component/Personnage.tsx
+++ src/component/Personnage.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Personnage: React.FC = () => {
   return (
-    <div id="1018820439746478081" className='personnage'></div>
+    <div id="1018820439746478081" className='personnage' style={{top: '20%', left: '20%'}}>
+      <p className='nomPersonnage'></p>
+    </div>
   );
 };
 
