9581f16d6e26134ce65f643d2c39a3775a7a4b20
Joel Tidjane <tidjanejoel@gmail.com>
2024-11-21 11:34:36 -0500

    Ajout du eventlistener sur le personnage

--- src/pages/Salles/Lobby.tsx
+++ src/pages/Salles/Lobby.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Personnage from '../../component/Personnage';
 import Background from '../../assets/img/vieux-plancher-sale.jpg';
 import initializePositions from '../../component/Positions';
@@ -6,20 +6,22 @@ import Messagerie from '../../component/Messagerie';
 import { useCookies } from 'react-cookie';
 
 const Lobby: React.FC = () => {
-  const [cookies] = useCookies();
+    const [cookies] = useCookies();
 
-  useEffect(() => {
-    initializePositions(cookies.id);
-  }, [cookies]);
-  
-  return (
-    <div className='conteneur-jeu'>
-      <div className='contenu-jeu' style={{backgroundImage: `url(${Background})`}}>
-        <Personnage userId={cookies.id} />
-        <Messagerie></Messagerie>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        initializePositions(cookies.id);
+        
+
+    }, [cookies]);
+        
+        return (
+            <div className='conteneur-jeu'>
+            <div className='contenu-jeu' style={{backgroundImage: `url(${Background})`}}  >
+                <Personnage userId={cookies.id}  />
+                <Messagerie></Messagerie>
+            </div>
+            </div>
+    );
 };
 
 export default Lobby;
