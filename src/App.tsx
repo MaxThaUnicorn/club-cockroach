4b05c3fd2c9f8d0a9e474d77ee930c0882862e02
Felix Barre <202263237@cegepsherbrooke.qc.ca>
2024-11-21 12:27:09 -0500

    Username storage

--- src/App.tsx
+++ src/App.tsx
@@ -12,61 +12,53 @@ import Register from './pages/Register';
 import Garage from './pages/Salles/Garage';
 import SalleBain from './pages/Salles/SalleBain';
 import TerrainExt from './pages/Salles/TerrainExt';
-import { CookiesProvider } from 'react-cookie';
-import { useCookies } from 'react-cookie';
 
 const App: React.FC = () => { 
   const location = useLocation();
   const pageAccueil = location.pathname === '/';
-  const [cookies, setCookie, removeCookie] = useCookies(['id', 'username', 'email']);
   const navigate = useNavigate(); 
 
   const handleLogout = () => {
-    removeCookie('id', { path: '/' , domain:'localhost'});
-    removeCookie('username', { path: '/', domain:'localhost' });
-    removeCookie('email', { path: '/' , domain:'localhost'});
     navigate('/connexion'); 
   };
 
   return (
-    <CookiesProvider>
-        <div className='h-100 flex-column'>
-        
-            <nav style={{ display: pageAccueil ? 'none' : 'flex' }}>
-                <div>
-                <Link to="/"><img src={cockroachImg} alt="icone" /></Link>
-                <Link to="/">Accueil</Link>
-                <Link to="/propos">À propos</Link>
-                <Link to="/salle/lobby">Lobby</Link>
+    <div className='h-100 flex-column'>
+    
+        <nav style={{ display: pageAccueil ? 'none' : 'flex' }}>
+            <div>
+            <Link to="/"><img src={cockroachImg} alt="icone" /></Link>
+            <Link to="/">Accueil</Link>
+            <Link to="/propos">À propos</Link>
+            <Link to="/salle/lobby">Lobby</Link>
+            </div>
+            {sessionStorage.getItem('username') ? 
+            (
+                <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                    <span>Bienvenue, {sessionStorage.getItem('username')}!</span>
+                
+                    <button onClick={handleLogout} style={{ border: 'none', background: 'none', color:'#fff', cursor: 'pointer' }}>
+                    Déconnexion
+                    </button>
                 </div>
-                {cookies.username ? 
-                (
-                    <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                        <span>Bienvenue, {cookies.username}!</span>
-                    
-                        <button onClick={handleLogout} style={{ border: 'none', background: 'none', color:'#fff', cursor: 'pointer' }}>
-                        Déconnexion
-                        </button>
-                    </div>
-                ) : (
-                    <Link to="/connexion">Connexion</Link>
-                )}
-            </nav>
+            ) : (
+                <Link to="/connexion">Connexion</Link>
+            )}
+        </nav>
 
-            <Routes>
-                <Route path={ROUTES.ACCUEIL} element={<Accueil />} />
-                <Route path={ROUTES.CONNEXION} element={<Connexion />} />
-                <Route path={ROUTES.PROPOS} element={<Propos />} />
-                <Route path={ROUTES.GARAGE} element={<Garage />} />
-                <Route path={ROUTES.LOBBY} element={<Lobby />} />
-                <Route path={ROUTES.REGISTER} element={<Register />} />
-                <Route path={ROUTES.SALLEBAIN} element={<SalleBain />} />
-                <Route path={ROUTES.TERRAINEXT} element={<TerrainExt />} />
+        <Routes>
+            <Route path={ROUTES.ACCUEIL} element={<Accueil />} />
+            <Route path={ROUTES.CONNEXION} element={<Connexion />} />
+            <Route path={ROUTES.PROPOS} element={<Propos />} />
+            <Route path={ROUTES.GARAGE} element={<Garage />} />
+            <Route path={ROUTES.LOBBY} element={<Lobby />} />
+            <Route path={ROUTES.REGISTER} element={<Register />} />
+            <Route path={ROUTES.SALLEBAIN} element={<SalleBain />} />
+            <Route path={ROUTES.TERRAINEXT} element={<TerrainExt />} />
 
-                <Route path="*" element={<Page404 />} />
-            </Routes>
-        </div>
-        </CookiesProvider>
+            <Route path="*" element={<Page404 />} />
+        </Routes>
+    </div>
   );
 };
 
